7234c726867998082f94aff4036099449094b4ff
Finley AMO <finleyyowen@gmail.com>
2025-09-23 11:34:49 +1000

    Initial commit; project setup

---
+++ TEMPLATE.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file TEMPLATE.hpp
+ *
+ * @brief Template header file to enforce header comments and header guards.
+ *
+ * Copyright Finley Owen, 2025. All rights reserved.
+ */
+
+#ifndef __TEMPLATE_HPP__
+#define __TEMPLATE_HPP__
+
+#endif
