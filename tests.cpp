26b70f1e62b8343a0a5f004d6d27b94353a65518
Finley AMO <finleyyowen@gmail.com>
2025-09-24 20:42:47 +1000

    Wrote proper tests for the lexer

--- tests.cpp
+++ tests.cpp
@@ -10,6 +10,8 @@
 #include "lexer.hpp"
 
 #include <iostream>
+#include <sstream>
+#include <cassert>
 using namespace std;
 
 // unsigned integer token
@@ -107,9 +109,100 @@ void lexer_debug(void)
     delete lexer;
 }
 
+// check the behaviour of the lexer when handling overlapping token types
+void lexer_test1(void)
+{
+    // create a lexer and register token types
+    Lexer *lexer = new Lexer();
+    lexer->registerTokenType(&WhitespaceToken::tokenType);
+    lexer->registerTokenType(&UIntToken::tokenType);
+    lexer->registerTokenType(&IntToken::tokenType);
+
+    // lex program string
+    string s = "12 -24 65 -2 44 -67";
+    lexer->lex(s);
+
+    string candidatesString = lexer->candidatesString();
+    istringstream ss = istringstream(candidatesString);
+
+    // check program output
+    string line;
+    assert(getline(ss, line));
+    assert(line == "uint token: \"12\"");
+    assert(getline(ss, line));
+    assert(line == "whitespace token: \" \"");
+    assert(getline(ss, line));
+    assert(line == "int token: \"-24\"");
+    assert(getline(ss, line));
+    assert(line == "whitespace token: \" \"");
+    assert(getline(ss, line));
+    assert(line == "uint token: \"65\"");
+    assert(getline(ss, line));
+    assert(line == "whitespace token: \" \"");
+    assert(getline(ss, line));
+    assert(line == "int token: \"-2\"");
+    assert(getline(ss, line));
+    assert(line == "whitespace token: \" \"");
+    assert(getline(ss, line));
+    assert(line == "uint token: \"44\"");
+    assert(getline(ss, line));
+    assert(line == "whitespace token: \" \"");
+    assert(getline(ss, line));
+    assert(line == "int token: \"-67\"");
+
+    // cleanup
+    delete lexer;
+}
+
+// check the behaviour of the lexer when handling unmatched input
+void lexer_test2()
+{
+
+    // create a lexer and register token types (note: don't register the
+    // whitespace token)
+    Lexer *lexer = new Lexer();
+    lexer->registerTokenType(&UIntToken::tokenType);
+    lexer->registerTokenType(&IntToken::tokenType);
+
+    // program string
+    string s = "12 -24 65 -2 44 -67";
+
+    // note the expected behaviour is a runtime error
+    bool behavedAsExpected;
+
+    try
+    {
+        // lex program string
+        lexer->lex(s);
+
+        // uh oh - that should have thrown a runtime error since the spaces are
+        // unmatched
+        behavedAsExpected = false;
+    }
+    catch (runtime_error &e)
+    {
+        // yay - threw a runtime error when a runtime error was expected
+        behavedAsExpected = true;
+    }
+
+    // throw a runtime error if the behaviour was unmatched - this is a little
+    // confusing since we just handled a runtime error, but this error is thrown
+    // if lexer->lex(s) does not throw a runtime error, and this error is not
+    // handled in the code and will crash the test program
+    if (!behavedAsExpected)
+    {
+        throw runtime_error("Didn't throw runtime error on unmatched input!");
+    }
+
+    delete lexer;
+}
+
+// program entry - run all tests and debug if required
 int main(void)
 {
-    lexer_debug();
+    lexer_test1();
+    lexer_test2();
+    // lexer_debug();
 }
 
 //
